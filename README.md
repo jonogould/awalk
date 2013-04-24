9091916be791473802a0db1b4a22ae0e0406ba10
Jono Gould <jono@gould.co.za>
2013-04-24 10:23:03 +0200

    Get it up

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-awalk
+Awalk
 =====
 
-Simple asynchronous recursive file walker.
+An asynchronous recursive file walker that returns an array of files (filename, path and file size).
+
+
+## Installation
+
+First, you need to install [node](http://nodejs.org). Then you can continue!
+
+This script runs in terminal.
+
+To get these tools running, you need the following dependencies, available off npm:
+
+- [underscorejs](http://underscorejs.org)
+
+To install this dependency the easy way, run the following command from the ``` awalk ``` root dir:
+
+``` $ npm init ```
+
+Or place the dependencies there manually but adding them to a ``` node_modules ``` folder
+
+Or add them by calling:
+
+``` $ npm install underscore ```
+
+
+## Usage
+
+How do you use ``` awalk ```, I hear you asking? Well:
+
+- Open terminal
+- Navigate to the ``` awalk ``` root dir
+- Type ``` ./awalk ``` and press enter
+- There are your helpful instructions :D
+
+
+### Now go play, use it to create useful stuff!
---
+++ awalk.js
@@ -0,0 +1,64 @@
+//	Dependancies
+var fs = require('fs');
+var _ = require('underscore');
+
+
+/*
+*	The big walk
+*	============
+*	Asynchronously lists all files (filename and full path)
+*	from within every folder of the specified dir.
+*
+*	Returns = array of objects (filename, full path)
+*
+*	dir 	= root folder
+*	done 	= callback that is triggered on completion
+*/
+
+var awalk = function(dir, done) {
+	//	The results array
+	var results = [];
+
+	//	Start off by reading dir, return list
+	fs.readdir(dir, function(err, list) {
+		//	Stop and report an error, if there is one
+		if (err)
+			return done(err);
+
+		var pending = list.length;
+		
+		if (!pending)
+			return done(err, results);
+
+		//	Run through the list of files
+		_.each(list, function(file) {
+			//	We want the full path
+			var path = dir + '/' + file;
+
+			//	Get the details of the file
+			fs.stat(path, function(err, stat) {
+				//	If the file is legit and can spit out some stats
+				if (stat) {
+					if (stat.isDirectory()) {
+						//	If file is a directory, inception!
+						awalk(path, function(err, res) {
+							results = results.concat(res);
+							
+							if (!--pending)
+								done(err, results);
+						});
+					}
+					else {
+						//	If file is a file
+						results.push({filename: file, path: path, size: stat.size});
+						
+						if (!--pending)
+							done(err, results);
+					}
+				}
+			});
+		});
+	});
+};
+
+exports.awalk = awalk;
